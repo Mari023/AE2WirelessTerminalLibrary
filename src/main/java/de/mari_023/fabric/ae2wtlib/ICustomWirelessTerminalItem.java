839858e854d8fc187dc65b95bc862de4614ffdc4
mari_023 <Mari_023>
2021-02-23 23:06:57 +0100

    begin implementation for curios

--- src/main/java/de/mari_023/fabric/ae2wtlib/ICustomWirelessTerminalItem.java
+++ src/main/java/de/mari_023/fabric/ae2wtlib/ICustomWirelessTerminalItem.java
@@ -1,7 +1,8 @@
 package de.mari_023.fabric.ae2wtlib;
 
 import appeng.api.features.IWirelessTermHandler;
+import top.theillusivec4.curios.api.type.component.ICurio;
 
-public interface ICustomWirelessTerminalItem extends IWirelessTermHandler {//TODO add support for other energy api and curious
+public interface ICustomWirelessTerminalItem extends IWirelessTermHandler, ICurio {//TODO add support for other energy api and make curious actually work
 
 }
--- src/main/java/de/mari_023/fabric/ae2wtlib/ae2wtlib.java
+++ src/main/java/de/mari_023/fabric/ae2wtlib/ae2wtlib.java
@@ -2,6 +2,7 @@ package de.mari_023.fabric.ae2wtlib;
 
 import appeng.container.AEBaseContainer;
 import appeng.container.ContainerOpener;
+import nerdhub.cardinal.components.api.event.ItemComponentCallbackV2;
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.client.itemgroup.FabricItemGroupBuilder;
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
@@ -12,6 +13,8 @@ import net.minecraft.item.Items;
 import net.minecraft.screen.ScreenHandlerType;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
+import top.theillusivec4.curios.api.CuriosComponent;
+import top.theillusivec4.curios.api.type.component.ICurio;
 
 public class ae2wtlib implements ModInitializer {
 
@@ -23,6 +26,9 @@ public class ae2wtlib implements ModInitializer {
     public void onInitialize() {
         Registry.register(Registry.ITEM, new Identifier("ae2wtlib", "wireless_universal_terminal"), UNIVERSAL_TERMINAL);
         WUTContainer.TYPE = registerScreenHandler(WUTContainer::fromNetwork, WUTContainer::open);
+        ItemComponentCallbackV2.event(UNIVERSAL_TERMINAL).register(((item, itemStack, componentContainer) -> componentContainer.put(CuriosComponent.ITEM, new ICurio() {
+
+        })));
     }
 
     public static <T extends AEBaseContainer> ScreenHandlerType<T> registerScreenHandler(ScreenHandlerRegistry.ExtendedClientHandlerFactory<T> factory, ContainerOpener.Opener<T> opener) {
