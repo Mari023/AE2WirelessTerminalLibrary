767d5678ec9def950b417363b59e1926b4404f37
Mari023 <38946771+Mari023@users.noreply.github.com>
2021-03-18 22:34:28 +0100

    make the crafting terminal show up as crafting table

--- src/main/java/de/mari_023/fabric/ae2wtlib/rei/Plugin.java
+++ src/main/java/de/mari_023/fabric/ae2wtlib/rei/Plugin.java
@@ -1,9 +1,12 @@
 package de.mari_023.fabric.ae2wtlib.rei;
 
+import de.mari_023.fabric.ae2wtlib.ae2wtlib;
 import de.mari_023.fabric.ae2wtlib.wct.WCTContainer;
 import de.mari_023.fabric.ae2wtlib.wpt.WPTContainer;
+import me.shedaniel.rei.api.EntryStack;
 import me.shedaniel.rei.api.RecipeHelper;
 import me.shedaniel.rei.api.plugins.REIPluginV0;
+import me.shedaniel.rei.plugin.DefaultPlugin;
 import net.minecraft.util.Identifier;
 
 public class Plugin implements REIPluginV0 {
@@ -17,5 +20,7 @@ public class Plugin implements REIPluginV0 {
     public void registerOthers(RecipeHelper recipeHelper) {
         recipeHelper.registerAutoCraftingHandler(new CraftingRecipeTransferHandler(WCTContainer.class));
         recipeHelper.registerAutoCraftingHandler(new PatternRecipeTransferHandler(WPTContainer.class));
+
+        recipeHelper.registerWorkingStations(DefaultPlugin.CRAFTING, EntryStack.create(ae2wtlib.CRAFTING_TERMINAL));
     }
 }
