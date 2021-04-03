e1873b325955cde1c1d234ccfd1e313ed86d2202
Mari023 <38946771+Mari023@users.noreply.github.com>
2021-04-03 23:52:38 +0200

    clean up

--- src/main/java/de/mari_023/fabric/ae2wtlib/wct/ItemWCT.java
+++ src/main/java/de/mari_023/fabric/ae2wtlib/wct/ItemWCT.java
@@ -2,10 +2,9 @@ package de.mari_023.fabric.ae2wtlib.wct;
 
 import appeng.container.ContainerLocator;
 import appeng.core.AEConfig;
-import de.mari_023.fabric.ae2wtlib.terminal.IInfinityBoosterCardHolder;
-import de.mari_023.fabric.ae2wtlib.terminal.ItemInfinityBooster;
-import de.mari_023.fabric.ae2wtlib.terminal.ItemWT;
 import de.mari_023.fabric.ae2wtlib.ae2wtlib;
+import de.mari_023.fabric.ae2wtlib.terminal.IInfinityBoosterCardHolder;
+import de.mari_023.fabric.ae2wtlib.terminal.ItemWT;
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.ItemStack;
--- src/main/java/de/mari_023/fabric/ae2wtlib/wpt/ItemWPT.java
+++ src/main/java/de/mari_023/fabric/ae2wtlib/wpt/ItemWPT.java
@@ -2,13 +2,11 @@ package de.mari_023.fabric.ae2wtlib.wpt;
 
 import appeng.container.ContainerLocator;
 import appeng.core.AEConfig;
-import de.mari_023.fabric.ae2wtlib.terminal.IInfinityBoosterCardHolder;
-import de.mari_023.fabric.ae2wtlib.terminal.ItemInfinityBooster;
-import de.mari_023.fabric.ae2wtlib.terminal.ItemWT;
 import de.mari_023.fabric.ae2wtlib.ae2wtlib;
+import de.mari_023.fabric.ae2wtlib.terminal.IInfinityBoosterCardHolder;
+import de.mari_023.fabric.ae2wtlib.terminal.ItemWT;
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
 import net.minecraft.entity.player.PlayerEntity;
-import net.minecraft.item.ItemStack;
 import net.minecraft.util.Hand;
 
 public class ItemWPT extends ItemWT implements IInfinityBoosterCardHolder {
