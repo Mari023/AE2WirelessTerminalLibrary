2851ffafa19f76f42ddd467ecdbb6d8ee13fd1b3
Mari023 <38946771+Mari023@users.noreply.github.com>
2021-04-14 12:19:20 +0200

    add tooltip for infinity booster card

--- src/main/java/de/mari_023/fabric/ae2wtlib/terminal/ItemInfinityBooster.java
+++ src/main/java/de/mari_023/fabric/ae2wtlib/terminal/ItemInfinityBooster.java
@@ -1,11 +1,27 @@
 package de.mari_023.fabric.ae2wtlib.terminal;
 
 import de.mari_023.fabric.ae2wtlib.ae2wtlib;
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.minecraft.client.item.TooltipContext;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.text.Text;
+import net.minecraft.text.TranslatableText;
+import net.minecraft.world.World;
+
+import java.util.List;
 
 public class ItemInfinityBooster extends Item {
     public ItemInfinityBooster() {
         super(new FabricItemSettings().group(ae2wtlib.ITEM_GROUP).maxCount(1));
+    }
+
+    @Override
+    @Environment(EnvType.CLIENT)
+    public void appendTooltip(final ItemStack stack, final World world, final List<Text> lines, final TooltipContext advancedTooltips) {
+        super.appendTooltip(stack, world, lines, advancedTooltips);
+        lines.add(new TranslatableText("item.ae2wtlib.infinity_booster_card.desc"));
     }
 }
