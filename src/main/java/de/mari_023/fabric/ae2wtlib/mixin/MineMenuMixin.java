813d611933a4813296a1e4187c218cfed7e55d1b
Mari023 <38946771+Mari023@users.noreply.github.com>
2021-05-11 22:00:45 +0200

    open Wireless Terminals from MineMenu

--- src/main/java/de/mari_023/fabric/ae2wtlib/mixin/MineMenuMixin.java
+++ src/main/java/de/mari_023/fabric/ae2wtlib/mixin/MineMenuMixin.java
@@ -7,7 +7,7 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.gen.Accessor;
 
 @Environment(EnvType.CLIENT)
-@Mixin(MineMenuSelectScreen.class)
+@Mixin(value = MineMenuSelectScreen.class, remap = false)
 public interface MineMenuMixin {
     @Accessor("circleEntries")
     void setCircleEntries(int circleEntries);
