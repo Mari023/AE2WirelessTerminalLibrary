c07184a946efa02ab1d0b54b23e14871c78d0a6a
Mari023 <38946771+Mari023@users.noreply.github.com>
2021-05-14 17:15:13 +0200

    fix slot repositioning

--- src/main/java/de/mari_023/fabric/ae2wtlib/wpt/WPTContainer.java
+++ src/main/java/de/mari_023/fabric/ae2wtlib/wpt/WPTContainer.java
@@ -31,6 +31,7 @@ import appeng.util.inv.InvOperation;
 import appeng.util.inv.WrapperCursorItemHandler;
 import appeng.util.item.AEItemStack;
 import de.mari_023.fabric.ae2wtlib.Config;
+import de.mari_023.fabric.ae2wtlib.mixin.SlotMixin;
 import de.mari_023.fabric.ae2wtlib.util.ContainerHelper;
 import de.mari_023.fabric.ae2wtlib.terminal.FixedWTInv;
 import de.mari_023.fabric.ae2wtlib.terminal.IWTInvHolder;
