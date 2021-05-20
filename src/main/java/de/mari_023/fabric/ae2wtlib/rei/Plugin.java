cc7c5ae5378d3d63ea1f8b7dd23ca5a775bd91e1
Mari023 <38946771+Mari023@users.noreply.github.com>
2021-05-20 16:45:42 +0200

    show wut recipes in REI

--- src/main/java/de/mari_023/fabric/ae2wtlib/rei/Plugin.java
+++ src/main/java/de/mari_023/fabric/ae2wtlib/rei/Plugin.java
@@ -3,6 +3,8 @@ package de.mari_023.fabric.ae2wtlib.rei;
 import de.mari_023.fabric.ae2wtlib.ae2wtlib;
 import de.mari_023.fabric.ae2wtlib.wct.WCTContainer;
 import de.mari_023.fabric.ae2wtlib.wpt.WPTContainer;
+import de.mari_023.fabric.ae2wtlib.wut.recipe.Combine;
+import de.mari_023.fabric.ae2wtlib.wut.recipe.Upgrade;
 import me.shedaniel.rei.api.EntryStack;
 import me.shedaniel.rei.api.RecipeHelper;
 import me.shedaniel.rei.api.plugins.REIPluginV0;
@@ -22,5 +24,12 @@ public class Plugin implements REIPluginV0 {
         recipeHelper.registerAutoCraftingHandler(new PatternRecipeTransferHandler(WPTContainer.class));
 
         recipeHelper.registerWorkingStations(DefaultPlugin.CRAFTING, EntryStack.create(ae2wtlib.CRAFTING_TERMINAL));
+    }
+
+
+    @Override
+    public void registerRecipeDisplays(RecipeHelper recipeHelper) {
+        recipeHelper.registerRecipes(DefaultPlugin.CRAFTING, Combine.class, CombineDisplay::new);
+        recipeHelper.registerRecipes(DefaultPlugin.CRAFTING, Upgrade.class, UpgradeDisplay::new);
     }
 }
