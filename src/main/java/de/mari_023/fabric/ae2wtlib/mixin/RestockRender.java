d494613999e5d639cfa858a81e6118ef85293a52
Mari023 <38946771+Mari023@users.noreply.github.com>
2021-06-12 01:23:05 +0200

    Inject instead of Overwriting

--- src/main/java/de/mari_023/fabric/ae2wtlib/mixin/RestockRender.java
+++ src/main/java/de/mari_023/fabric/ae2wtlib/mixin/RestockRender.java
@@ -9,22 +9,21 @@ import net.minecraft.client.font.TextRenderer;
 import net.minecraft.client.render.item.ItemRenderer;
 import net.minecraft.item.ItemStack;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Overwrite;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Environment(EnvType.CLIENT)
 @Mixin(ItemRenderer.class)
 public class RestockRender {
-    /**
-     * @author Mari_023
-     */
-    @Overwrite
-    public void renderGuiItemOverlay(TextRenderer renderer, ItemStack stack, int x, int y) {
+
+    @Inject(method = "renderGuiItemOverlay(Lnet/minecraft/client/font/TextRenderer;Lnet/minecraft/item/ItemStack;II)V", at = @At(value = "INVOKE"), cancellable = true)
+    public void renderGuiItemOverlay(TextRenderer renderer, ItemStack stack, int x, int y, CallbackInfo ci) {
         if(MinecraftClient.getInstance().player == null) return;
         CraftingTerminalHandler handler = CraftingTerminalHandler.getCraftingTerminalHandler(MinecraftClient.getInstance().player);
-        if(MinecraftClient.getInstance().player.isCreative() || !ItemWT.getBoolean(handler.getCraftingTerminal(), "restock") || !handler.inRange()) {
-            ((ItemRenderer) (Object) this).renderGuiItemOverlay(renderer, stack, x, y, null);
+        if(MinecraftClient.getInstance().player.isCreative() || !ItemWT.getBoolean(handler.getCraftingTerminal(), "restock") || !handler.inRange())
             return;
-        }
         ((ItemRenderer) (Object) this).renderGuiItemOverlay(renderer, stack, x, y, "Restock");
+        ci.cancel();
     }
 }
