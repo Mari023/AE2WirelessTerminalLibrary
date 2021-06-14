fad7e0a7f526ea3f6e812877521a3f0d9131fe71
Mari023 <38946771+Mari023@users.noreply.github.com>
2021-06-14 23:21:58 +0200

    some optimizations and allow longs all the way

--- src/main/java/de/mari_023/fabric/ae2wtlib/wct/magnet_card/MagnetHandler.java
+++ src/main/java/de/mari_023/fabric/ae2wtlib/wct/magnet_card/MagnetHandler.java
@@ -3,6 +3,7 @@ package de.mari_023.fabric.ae2wtlib.wct.magnet_card;
 import appeng.api.networking.storage.IStorageGrid;
 import appeng.api.storage.channels.IItemStorageChannel;
 import appeng.api.storage.data.IAEItemStack;
+import appeng.api.storage.data.IItemList;
 import appeng.core.Api;
 import appeng.me.cache.NetworkMonitor;
 import appeng.util.item.AEItemStack;
@@ -11,7 +12,6 @@ import de.mari_023.fabric.ae2wtlib.wct.CraftingTerminalHandler;
 import net.fabricmc.fabric.api.networking.v1.PacketByteBufs;
 import net.fabricmc.fabric.api.networking.v1.ServerPlayNetworking;
 import net.minecraft.entity.ItemEntity;
-import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.network.PacketByteBuf;
@@ -30,11 +30,8 @@ public class MagnetHandler {
         for(ServerPlayerEntity player : playerList) {
             if(ItemMagnetCard.isActiveMagnet(CraftingTerminalHandler.getCraftingTerminalHandler(player).getCraftingTerminal())) {
                 List<ItemEntity> entityItems = player.getServerWorld().getEntitiesByClass(ItemEntity.class, player.getBoundingBox().expand(16.0D), EntityPredicates.VALID_ENTITY);
-                for(ItemEntity entityItemNearby : entityItems) {
-                    if(!player.isSneaking()) {
-                        entityItemNearby.onPlayerCollision(player);
-                    }
-                }
+                for(ItemEntity entityItemNearby : entityItems)
+                    if(!player.isSneaking()) entityItemNearby.onPlayerCollision(player);
             }
             sendRestockAble(player);
         }
@@ -46,15 +43,18 @@ public class MagnetHandler {
     public void sendRestockAble(ServerPlayerEntity player) {
         CraftingTerminalHandler handler = CraftingTerminalHandler.getCraftingTerminalHandler(player);
         boolean canRestock = !player.isCreative() && ItemWT.getBoolean(handler.getCraftingTerminal(), "restock") && handler.inRange();
-        HashMap<Item, Integer> items = new HashMap<>();
+        HashMap<Item, Long> items = new HashMap<>();
+
+        IItemList<IAEItemStack> storageList = ((NetworkMonitor<IAEItemStack>) ((IStorageGrid) handler.getTargetGrid().getCache(IStorageGrid.class)).getInventory(Api.instance().storage().getStorageChannel(IItemStorageChannel.class))).getStorageList();
+
         if(canRestock) for(int i = 0; i < player.inventory.size(); i++) {
             ItemStack stack = player.inventory.getStack(i);
             if(stack.isEmpty()) continue;
-            if(!items.containsKey(stack.getItem())) items.put(stack.getItem(), getCount(player, stack));
+            if(!items.containsKey(stack.getItem())) items.put(stack.getItem(), getCount(storageList, stack));
         }
 
         PacketByteBuf buf = PacketByteBufs.create();
-        for(Map.Entry<Item, Integer> entry : items.entrySet())
+        for(Map.Entry<Item, Long> entry : items.entrySet())
             AEItemStack.fromItemStack(new ItemStack(entry.getKey())).setStackSize(entry.getValue()).writeToPacket(buf);
         if(canRestock || sendEmpty)
             ServerPlayNetworking.send(player, new Identifier("ae2wtlib", "restock_amounts"), buf);
@@ -62,12 +62,8 @@ public class MagnetHandler {
         else if(sendEmpty) sendEmpty = false;
     }
 
-    private int getCount(PlayerEntity player, ItemStack stack) {
-        CraftingTerminalHandler handler = CraftingTerminalHandler.getCraftingTerminalHandler(player);
-        NetworkMonitor<IAEItemStack> networkMonitor = (NetworkMonitor<IAEItemStack>) ((IStorageGrid) handler.getTargetGrid().getCache(IStorageGrid.class)).getInventory(Api.instance().storage().getStorageChannel(IItemStorageChannel.class));
-        IAEItemStack aeStack = networkMonitor.getStorageList().findPrecise(AEItemStack.fromItemStack(stack));
-        int i = 0;
-        if(aeStack != null) i = (int) Math.min(aeStack.getStackSize(), Integer.MAX_VALUE);
-        return i;
+    private long getCount(IItemList<IAEItemStack> storageList, ItemStack stack) {
+        IAEItemStack aeStack = storageList.findPrecise(AEItemStack.fromItemStack(stack));
+        return aeStack == null ? 0 : aeStack.getStackSize();
     }
 }
