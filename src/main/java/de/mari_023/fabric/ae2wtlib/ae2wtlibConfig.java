b9dce1f0b0b70793bb84a86330323224935312f1
Mari023 <38946771+Mari023@users.noreply.github.com>
2021-06-22 00:19:20 +0200

    Integrate Config with ModMenu

--- src/main/java/de/mari_023/fabric/ae2wtlib/ae2wtlibConfig.java
+++ src/main/java/de/mari_023/fabric/ae2wtlib/ae2wtlibConfig.java
@@ -2,18 +2,28 @@ package de.mari_023.fabric.ae2wtlib;
 
 import appeng.core.AEConfig;
 import me.shedaniel.autoconfig.ConfigData;
+import me.shedaniel.autoconfig.annotation.ConfigEntry;
 import net.fabricmc.loader.api.FabricLoader;
 
 @me.shedaniel.autoconfig.annotation.Config(name = "ae2wtlib")
 public class ae2wtlibConfig implements ConfigData {
 
+    //@ConfigEntry.Gui.Tooltip
     private double chargeRate = 8000;
+    //@ConfigEntry.Gui.Tooltip
     private double wutChargeRateMultiplier = 1;
+    @ConfigEntry.Gui.Tooltip
     private double outOfRangePowerMultiplier = 2;
-    private boolean enableTrinket = true;
+    //@ConfigEntry.Gui.Tooltip
     private double magnetCardRange = 16;
 
+    @ConfigEntry.Gui.Tooltip(count = 2)
+    private boolean enableTrinket = true;
+
+    @ConfigEntry.Gui.Excluded
     public static ae2wtlibConfig INSTANCE;
+
+    @ConfigEntry.Gui.Excluded
     private static boolean mineMenuChecked, mineMenuPresent, trinketPresent, trinketChecked;
 
     public double getPowerMultiplier(double range, boolean isOutOfRange) {
