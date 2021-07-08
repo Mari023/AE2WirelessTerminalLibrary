4c3257b5d76d97acce7c1133f7b673991d30eab0
Mari023 <38946771+Mari023@users.noreply.github.com>
2021-07-08 23:35:43 +0200

    optimize imports

--- src/main/java/de/mari_023/fabric/ae2wtlib/mixin/ItemTerminalScreenWireless.java
+++ src/main/java/de/mari_023/fabric/ae2wtlib/mixin/ItemTerminalScreenWireless.java
@@ -1,20 +1,9 @@
 package de.mari_023.fabric.ae2wtlib.mixin;
 
 import appeng.client.gui.me.items.ItemTerminalScreen;
-import appeng.container.me.crafting.WirelessCraftingStatusContainer;
-import de.mari_023.fabric.ae2wtlib.wct.WCTScreen;
-import de.mari_023.fabric.ae2wtlib.wpt.WPTScreen;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
-import net.fabricmc.fabric.api.client.networking.v1.ClientPlayNetworking;
-import net.fabricmc.fabric.api.networking.v1.PacketByteBufs;
-import net.minecraft.network.PacketByteBuf;
-import net.minecraft.util.Identifier;
-import net.minecraft.util.registry.Registry;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Environment(EnvType.CLIENT)
 @Mixin(value = ItemTerminalScreen.class, remap = false)
--- src/main/java/de/mari_023/fabric/ae2wtlib/mixin/MEInvUpdatePacket.java
+++ src/main/java/de/mari_023/fabric/ae2wtlib/mixin/MEInvUpdatePacket.java
@@ -1,21 +1,7 @@
 package de.mari_023.fabric.ae2wtlib.mixin;
 
-import appeng.api.storage.data.IAEItemStack;
-import appeng.core.sync.network.INetworkInfo;
 import appeng.core.sync.packets.MEInventoryUpdatePacket;
-import de.mari_023.fabric.ae2wtlib.util.WirelessCraftConfirmScreen;
-import net.fabricmc.api.EnvType;
-import net.fabricmc.api.Environment;
-import net.minecraft.client.MinecraftClient;
-import net.minecraft.entity.player.PlayerEntity;
-import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
-import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-
-import java.util.List;
 
 @Mixin(value = MEInventoryUpdatePacket.class, remap = false)
 public class MEInvUpdatePacket {
