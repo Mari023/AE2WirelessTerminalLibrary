8b1746ca22b8081c67e210722ef68e910ee3d35a
Mari023 <38946771+Mari023@users.noreply.github.com>
2021-10-17 01:01:12 +0200

    optimize imports

--- src/main/java/de/mari_023/fabric/ae2wtlib/wpt/WPTGuiObject.java
+++ src/main/java/de/mari_023/fabric/ae2wtlib/wpt/WPTGuiObject.java
@@ -14,7 +14,6 @@ import appeng.items.misc.FluidDummyItem;
 import appeng.parts.reporting.PatternTerminalPart;
 import appeng.util.inv.AppEngInternalInventory;
 import appeng.util.inv.InternalInventoryHost;
-import appeng.util.item.AEItemStack;
 import de.mari_023.fabric.ae2wtlib.ae2wtlib;
 import de.mari_023.fabric.ae2wtlib.terminal.ItemWT;
 import de.mari_023.fabric.ae2wtlib.terminal.WTGuiObject;
@@ -22,15 +21,12 @@ import de.mari_023.fabric.ae2wtlib.terminal.ae2wtlibInternalInventory;
 import net.fabricmc.fabric.api.networking.v1.PacketByteBufs;
 import net.fabricmc.fabric.api.networking.v1.ServerPlayNetworking;
 import net.minecraft.entity.player.PlayerEntity;
-import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.network.PacketByteBuf;
 import net.minecraft.screen.ScreenHandlerType;
 import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.util.Identifier;
 import org.jetbrains.annotations.Nullable;
-
-import java.util.Map;
 
 public class WPTGuiObject extends WTGuiObject implements IPortableCell, ISegmentedInventory, IViewCellStorage, InternalInventoryHost {
 
