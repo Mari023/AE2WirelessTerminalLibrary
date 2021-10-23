e021e226939098bcc25525c8f2a5155e07b90dfa
Domaman202 <vip.domaman@mail.ru>
2021-10-23 20:35:47 +0700

    remove usage of internal api

--- src/main/java/de/mari_023/fabric/ae2wtlib/rei/PatternRecipeTransferHandler.java
+++ src/main/java/de/mari_023/fabric/ae2wtlib/rei/PatternRecipeTransferHandler.java
@@ -2,7 +2,7 @@ package de.mari_023.fabric.ae2wtlib.rei;
 
 import de.mari_023.fabric.ae2wtlib.wpt.WPTContainer;
 import me.shedaniel.rei.api.common.display.Display;
-import me.shedaniel.rei.plugin.common.DefaultPlugin;
+import me.shedaniel.rei.plugin.common.BuiltinPlugin;
 import net.minecraft.text.TranslatableText;
 
 public class PatternRecipeTransferHandler extends RecipeTransferHandler<WPTContainer> {
@@ -14,7 +14,7 @@ public class PatternRecipeTransferHandler extends RecipeTransferHandler<WPTConta
 
     @Override
     protected Result doTransferRecipe(WPTContainer container, Display recipe) {
-        if(container.isCraftingMode() && recipe.getCategoryIdentifier() != DefaultPlugin.CRAFTING) {
+        if(container.isCraftingMode() && recipe.getCategoryIdentifier() != BuiltinPlugin.CRAFTING) {
             return Result.createFailed(new TranslatableText("jei.appliedenergistics2.requires_processing_mode"));
         }
         if(recipe.getOutputEntries().isEmpty()) {
