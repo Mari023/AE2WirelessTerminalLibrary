97c10bfea76368fa3ff676f93b4753296e08c92a
Mari023 <38946771+Mari023@users.noreply.github.com>
2021-11-14 01:28:04 +0100

    fix infinity booster card background

--- src/main/java/de/mari_023/fabric/ae2wtlib/wit/WITScreen.java
+++ src/main/java/de/mari_023/fabric/ae2wtlib/wit/WITScreen.java
@@ -1,6 +1,7 @@
 package de.mari_023.fabric.ae2wtlib.wit;
 
 import appeng.client.gui.me.interfaceterminal.InterfaceTerminalScreen;
+import appeng.client.gui.style.Blitter;
 import appeng.client.gui.style.ScreenStyle;
 import appeng.client.gui.style.StyleManager;
 import de.mari_023.fabric.ae2wtlib.wut.CycleTerminalButton;
@@ -33,7 +34,6 @@ public class WITScreen extends InterfaceTerminalScreen<WITContainer> implements 
     @Override
     public void drawBG(MatrixStack matrixStack, final int offsetX, final int offsetY, final int mouseX, final int mouseY, float partialTicks) {
         super.drawBG(matrixStack, offsetX, offsetY, mouseX, mouseY, partialTicks);
-        bindTexture("wtlib/guis/interface.png");
-        //FIXME draw background of infinity booster card slot?
+        Blitter.texture("wtlib/guis/interface.png").src(172, 128, 23, 24).dest(x + 172, y + backgroundHeight - 90).blit(matrixStack, getZOffset());
     }
 }
