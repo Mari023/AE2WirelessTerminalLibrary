8e2dd842e7aba1215809fa29b42e832c151a0b80
Mari023 <38946771+Mari023@users.noreply.github.com>
2021-12-19 00:04:47 +0100

    return the security terminal's node if there is no wireless access point in range

--- src/main/java/de/mari_023/fabric/ae2wtlib/terminal/WTMenuHost.java
+++ src/main/java/de/mari_023/fabric/ae2wtlib/terminal/WTMenuHost.java
@@ -1,9 +1,14 @@
 package de.mari_023.fabric.ae2wtlib.terminal;
 
+import appeng.api.features.Locatables;
+import appeng.api.networking.IGridNode;
 import appeng.helpers.WirelessTerminalMenuHost;
+import appeng.items.tools.powered.WirelessTerminalItem;
 import appeng.menu.ISubMenu;
 import de.mari_023.fabric.ae2wtlib.AE2wtlibConfig;
+
 import java.util.function.BiConsumer;
+
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.inventory.MenuType;
 import net.minecraft.world.item.ItemStack;
@@ -13,11 +18,21 @@ public abstract class WTMenuHost extends WirelessTerminalMenuHost {
     private final ViewCellInventory viewCellInventory;
     private final Player myPlayer;
     private boolean rangeCheck;
+    private IGridNode securityTerminalNode;
 
-    public WTMenuHost(final Player ep, int inventorySlot, final ItemStack is, BiConsumer<Player, ISubMenu> returnToMainMenu) {
-        super(ep, inventorySlot, is, returnToMainMenu);
+    public WTMenuHost(final Player player, int inventorySlot, final ItemStack is, BiConsumer<Player, ISubMenu> returnToMainMenu) {
+        super(player, inventorySlot, is, returnToMainMenu);
         viewCellInventory = new ViewCellInventory(is);
-        myPlayer = ep;
+        myPlayer = player;
+        var actionHost = Locatables.securityStations().get(player.level, ((WirelessTerminalItem) is.getItem()).getGridKey(is).getAsLong());
+        if(actionHost != null) securityTerminalNode = actionHost.getActionableNode();
+    }
+
+    @Override
+    public IGridNode getActionableNode() {
+        IGridNode node = super.getActionableNode();
+        if(node != null) return node;
+        return securityTerminalNode;
     }
 
     public abstract MenuType<?> getType();
