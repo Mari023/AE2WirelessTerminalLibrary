d4182b9a1e2a5ea628539d39fc52fdcf24e87c03
Mari023 <38946771+Mari023@users.noreply.github.com>
2022-01-06 03:32:35 +0100

    remove WTlibInternalInventory and store WirelessPatternEncodingTerminal Inventories like normal people store it

--- src/main/java/de/mari_023/fabric/ae2wtlib/wet/WETMenu.java
+++ src/main/java/de/mari_023/fabric/ae2wtlib/wet/WETMenu.java
@@ -5,7 +5,6 @@ import appeng.api.networking.IGridNode;
 import appeng.menu.implementations.MenuTypeBuilder;
 import appeng.menu.me.items.PatternEncodingTermMenu;
 import de.mari_023.fabric.ae2wtlib.terminal.IWTInvHolder;
-import de.mari_023.fabric.ae2wtlib.terminal.ItemWT;
 import de.mari_023.fabric.ae2wtlib.wut.ItemWUT;
 import net.minecraft.world.entity.player.Inventory;
 import net.minecraft.world.inventory.MenuType;
@@ -22,30 +21,6 @@ public class WETMenu extends PatternEncodingTermMenu implements IWTInvHolder {
     public WETMenu(int id, final Inventory ip, final WETMenuHost gui) {
         super(TYPE, id, ip, gui, true);
         WETGUIObject = gui;
-
-        if(isClient()) {//FIXME set craftingMode and substitute serverside
-            //WETGUIObject.setCraftingRecipe(ItemWT.getBoolean(WETGUIObject.getItemStack(), "craftingMode"));
-            setSubstitute(ItemWT.getBoolean(WETGUIObject.getItemStack(), "substitute"));
-            setSubstituteFluids(ItemWT.getBoolean(WETGUIObject.getItemStack(), "substitute_fluids"));
-        }
-    }
-
-    public boolean isSubstitute() {
-        return WETGUIObject.isSubstitution();
-    }
-
-    public void setSubstitute(boolean substitute) {
-        super.setSubstitute(substitute);
-        WETGUIObject.setSubstitution(substitute);
-    }
-
-    public boolean isSubstituteFluids() {
-        return WETGUIObject.isFluidSubstitution();
-    }
-
-    public void setSubstituteFluids(boolean substituteFluids) {
-        super.setSubstituteFluids(substituteFluids);
-        WETGUIObject.setFluidSubstitution(substituteFluids);
     }
 
     @Override
--- src/main/java/de/mari_023/fabric/ae2wtlib/wet/WETMenuHost.java
+++ src/main/java/de/mari_023/fabric/ae2wtlib/wet/WETMenuHost.java
@@ -6,15 +6,16 @@ import appeng.api.implementations.blockentities.IViewCellStorage;
 import appeng.api.inventories.ISegmentedInventory;
 import appeng.api.inventories.InternalInventory;
 import appeng.api.stacks.GenericStack;
+import appeng.crafting.pattern.AECraftingPattern;
+import appeng.crafting.pattern.AEProcessingPattern;
 import appeng.crafting.pattern.IAEPatternDetails;
 import appeng.helpers.IPatternTerminalHost;
 import appeng.menu.ISubMenu;
 import appeng.parts.encoding.EncodingMode;
 import appeng.util.inv.AppEngInternalInventory;
 import de.mari_023.fabric.ae2wtlib.AE2wtlib;
-import de.mari_023.fabric.ae2wtlib.terminal.ItemWT;
 import de.mari_023.fabric.ae2wtlib.terminal.WTMenuHost;
-import de.mari_023.fabric.ae2wtlib.terminal.WTlibInternalInventory;
+import net.minecraft.nbt.CompoundTag;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
@@ -25,27 +26,20 @@ import java.util.function.BiConsumer;
 public class WETMenuHost extends WTMenuHost implements ISegmentedInventory, IViewCellStorage, IPatternTerminalHost {
 
     private EncodingMode mode = EncodingMode.CRAFTING;
-    private boolean craftingMode = true;
     private boolean substitute = false;
     private boolean substituteFluids = true;
-    private final AppEngInternalInventory crafting;
-    private final AppEngInternalInventory output;
-    private final AppEngInternalInventory pattern;
+    private final AppEngInternalInventory crafting = new AppEngInternalInventory(this, 9);
+    private final AppEngInternalInventory output = new AppEngInternalInventory(this, 3);
+    private final AppEngInternalInventory pattern = new AppEngInternalInventory(this, 2);
 
     public WETMenuHost(final Player ep, @Nullable Integer inventorySlot, final ItemStack is, BiConsumer<Player, ISubMenu> returnToMainMenu) {
         super(ep, inventorySlot, is, returnToMainMenu);
-        crafting = new WTlibInternalInventory(this, 9, "pattern_crafting", is);
-        output = new WTlibInternalInventory(this, 3, "output", is);
-        pattern = new WTlibInternalInventory(this, 2, "pattern", is);
+        loadFromNbt();
     }
 
     @Override
     public ItemStack getMainMenuIcon() {
         return new ItemStack(AE2wtlib.PATTERN_ENCODING_TERMINAL);
-    }
-
-    public boolean isCraftingRecipe() {
-        return craftingMode;
     }
 
     @Override
@@ -67,17 +61,44 @@ public class WETMenuHost extends WTMenuHost implements ISegmentedInventory, IVie
         fixCraftingRecipes();
     }
 
+    private void loadFromNbt() {
+        CompoundTag tag = getItemStack().getOrCreateTag();
+        try {
+            mode = EncodingMode.valueOf(tag.getString("wet_mode"));
+        } catch(IllegalArgumentException ignored) {
+            mode = EncodingMode.CRAFTING;
+        }
+        setSubstitution(tag.getBoolean("wet_substitute"));
+        setFluidSubstitution(tag.getBoolean("wet_substituteFluids"));
+        pattern.readFromNBT(tag, "wet_pattern");
+        output.readFromNBT(tag, "wet_outputList");
+        crafting.readFromNBT(tag, "wet_craftingGrid");
+    }
+
     @Override
-    public void saveChanges() {}
+    public void saveChanges() {
+        CompoundTag tag = getItemStack().getOrCreateTag();
+        tag.putString("wet_mode", mode.name());
+        tag.putBoolean("wet_substitute", substitute);
+        tag.putBoolean("wet_substituteFluids", substituteFluids);
+        pattern.writeToNBT(tag, "wet_pattern");
+        output.writeToNBT(tag, "wet_outputList");
+        crafting.writeToNBT(tag, "wet_craftingGrid");
+    }
 
     @Override
     public void onChangeInventory(final InternalInventory inv, final int slot) {
         if(inv == pattern && slot == 1) {
             final ItemStack is = pattern.getStackInSlot(1);
-            final IPatternDetails details = PatternDetailsHelper.decodePattern(is, getPlayer().level);
+            final IPatternDetails details = PatternDetailsHelper.decodePattern(is, getPlayer().getLevel());
+            if(details instanceof AECraftingPattern) {
+                setMode(EncodingMode.CRAFTING);
+            } else if(details instanceof AEProcessingPattern) {
+                setMode(EncodingMode.PROCESSING);
+            }
             if(details instanceof IAEPatternDetails aeDetails) {
-                setCraftingRecipe(aeDetails.isCraftable());
                 setSubstitution(aeDetails.canSubstitute());
+                setFluidSubstitution(aeDetails.canSubstituteFluids());
 
                 for(int x = 0; x < crafting.size() && x < aeDetails.getSparseInputs().length; x++) {
                     crafting.setItemDirect(x, GenericStack.wrapInItemStack(aeDetails.getSparseInputs()[x]));
@@ -90,19 +111,12 @@ public class WETMenuHost extends WTMenuHost implements ISegmentedInventory, IVie
         } else if(inv == crafting) fixCraftingRecipes();
     }
 
-    public void setCraftingRecipe(final boolean craftingMode) {
-        this.craftingMode = craftingMode;
-        ItemWT.setBoolean(getItemStack(), craftingMode, "craftingMode");
-        if(!isClientSide()) fixCraftingRecipes();
-    }
-
     public boolean isSubstitution() {
         return substitute;
     }
 
     public void setSubstitution(final boolean canSubstitute) {
         substitute = canSubstitute;
-        ItemWT.setBoolean(getItemStack(), substitute, "substitute");
     }
 
     public boolean isFluidSubstitution() {
@@ -111,6 +125,5 @@ public class WETMenuHost extends WTMenuHost implements ISegmentedInventory, IVie
 
     public void setFluidSubstitution(final boolean canSubstitute) {
         substituteFluids = canSubstitute;
-        ItemWT.setBoolean(getItemStack(), substituteFluids, "substitute_fluids");
     }
 }
