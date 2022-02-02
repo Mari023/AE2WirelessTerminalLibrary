5c25fa55e921f85614c19f46f5c340939d680bf3
Mari023 <38946771+Mari023@users.noreply.github.com>
2022-02-02 19:03:05 +0100

    networking on forge is stupid architectury api to the rescue!

--- forge/src/main/java/de/mari_023/ae2wtlib/networking/forge/NetworkingManagerImpl.java
+++ forge/src/main/java/de/mari_023/ae2wtlib/networking/forge/NetworkingManagerImpl.java
@@ -1,24 +1,46 @@
 package de.mari_023.ae2wtlib.networking.forge;
 
+import net.minecraft.resources.ResourceLocation;
 import net.minecraft.server.level.ServerPlayer;
 
+import de.mari_023.ae2wtlib.AE2wtlib;
 import de.mari_023.ae2wtlib.networking.AE2wtlibPacket;
 import de.mari_023.ae2wtlib.networking.NetworkingManager;
+import dev.architectury.networking.NetworkManager;
 
 public class NetworkingManagerImpl {
     public static void registerServerBoundPacket(String name, NetworkingManager.PacketDeserializer deserializer) {
-        // FIXME implement
+        NetworkManager.registerReceiver(NetworkManager.c2s(), new ResourceLocation(AE2wtlib.MOD_NAME, name),
+                (buf, context) -> {
+                    buf.retain();
+                    context.queue(() -> {
+                        deserializer.create(buf).processPacketData(context.getPlayer());
+                        buf.release();
+                    });
+                });
     }
 
     public static void registerClientBoundPacket(String name, NetworkingManager.PacketDeserializer deserializer) {
-        // FIXME implement
+        NetworkManager.registerReceiver(NetworkManager.s2c(), new ResourceLocation(AE2wtlib.MOD_NAME, name),
+                (buf, context) -> {
+                    buf.retain();
+                    context.queue(() -> {
+                        if (context.getPlayer() == null)
+                            return;
+                        buf.retain();
+                        deserializer.create(buf).processPacketData(context.getPlayer());
+                        buf.release();
+                    });
+                });
     }
 
     public static void sendToClient(ServerPlayer player, AE2wtlibPacket packet) {
-        // FIXME implement
+        NetworkManager.sendToPlayer(player, new ResourceLocation(AE2wtlib.MOD_NAME, packet.getPacketName()),
+                packet.getPacketBuffer());
     }
 
     public static void sendToServer(AE2wtlibPacket packet) {
-        // FIXME implement
+        NetworkManager.sendToServer(new ResourceLocation(AE2wtlib.MOD_NAME, packet.getPacketName()),
+                packet.getPacketBuffer());
     }
 }
