9999fe275ea9cf6f69fe5d6a4afcb059dd655728
Mari023 <38946771+Mari023@users.noreply.github.com>
2022-02-03 19:48:27 +0100

    JEI integration WUT recipes don't work, but I don't care.

---
+++ forge/src/main/java/de/mari_023/ae2wtlib/jei/package-info.java
@@ -0,0 +1,7 @@
+@ParametersAreNonnullByDefault
+@MethodsReturnNonnullByDefault
+package de.mari_023.ae2wtlib.jei;
+
+import net.minecraft.MethodsReturnNonnullByDefault;
+
+import javax.annotation.ParametersAreNonnullByDefault;
