649166261dca72620712d6a65d2788f7214cbfa2
Mari023 <38946771+Mari023@users.noreply.github.com>
2022-02-08 16:34:04 +0100

    prepare for curios

--- common/src/main/java/de/mari_023/ae2wtlib/mixin/TrinketWCTContainer.java
+++
@@ -1,9 +0,0 @@
-package de.mari_023.ae2wtlib.mixin;
-
-import org.spongepowered.asm.mixin.Mixin;
-
-import de.mari_023.ae2wtlib.wct.WCTMenu;
-
-@Mixin(WCTMenu.class)
-public abstract class TrinketWCTContainer /* implements TrinketPlayerScreenHandler */ {
-}
