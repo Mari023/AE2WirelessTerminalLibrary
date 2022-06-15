7b7595342b6c345c0bf0912914d6005980bb8cec
Mari023 <38946771+Mari023@users.noreply.github.com>
2022-06-16 01:46:36 +0200

    update dependencies move Events to forge package so the packages don't conflict with common

--- forge/src/main/java/de/mari_023/ae2wtlib/forge/Events.java
+++ forge/src/main/java/de/mari_023/ae2wtlib/forge/Events.java
@@ -1,10 +1,11 @@
-package de.mari_023.ae2wtlib;
+package de.mari_023.ae2wtlib.forge;
 
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraftforge.event.TickEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 
+import de.mari_023.ae2wtlib.AE2wtlib;
 import de.mari_023.ae2wtlib.wct.magnet_card.MagnetHandler;
 
 @Mod.EventBusSubscriber(modid = AE2wtlib.MOD_NAME, bus = Mod.EventBusSubscriber.Bus.FORGE)
