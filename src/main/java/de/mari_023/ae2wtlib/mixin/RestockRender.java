37965cb0789b7a8606934b8da278349ca03856c9
Mari023 <38946771+Mari023@users.noreply.github.com>
2022-07-17 01:28:34 +0200

    Neoforge

--- src/main/java/de/mari_023/ae2wtlib/mixin/RestockRender.java
+++ src/main/java/de/mari_023/ae2wtlib/mixin/RestockRender.java
@@ -5,19 +5,19 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-import net.fabricmc.api.EnvType;
-import net.fabricmc.api.Environment;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.Font;
 import net.minecraft.client.gui.GuiGraphics;
 import net.minecraft.world.item.ItemStack;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.api.distmarker.OnlyIn;
 
 import de.mari_023.ae2wtlib.terminal.ItemWT;
 import de.mari_023.ae2wtlib.wct.CraftingTerminalHandler;
 
 import appeng.util.ReadableNumberConverter;
 
-@Environment(EnvType.CLIENT)
+@OnlyIn(Dist.CLIENT)
 @Mixin(GuiGraphics.class)
 public class RestockRender {
     @Inject(method = "renderItemDecorations(Lnet/minecraft/client/gui/Font;Lnet/minecraft/world/item/ItemStack;II)V", at = @At(value = "HEAD"), cancellable = true)
@@ -26,11 +26,14 @@ public class RestockRender {
             return;
         CraftingTerminalHandler handler = CraftingTerminalHandler
                 .getCraftingTerminalHandler(Minecraft.getInstance().player);
-        if (stack.getCount() == 1 || !handler.isRestockable(stack)
+        if (!handler.isRestockable(stack)
                 || !ItemWT.getBoolean(handler.getCraftingTerminal(), "restock") || !handler.inRange())
             return;
+        var amount = handler.getAccessibleAmount(stack);
+        if (amount == 1)
+            return;
         ((GuiGraphics) (Object) this).renderItemDecorations(font, stack, x, y,
-                ReadableNumberConverter.format(handler.getAccessibleAmount(stack), 3));
+                ReadableNumberConverter.format(amount, 3));
         ci.cancel();
     }
 }
