eee9d400620527bf459aea025314db0d72641191
Mari023 <38946771+Mari023@users.noreply.github.com>
2022-08-30 23:09:01 +0200

    tick the terminal in trinket slots

---
+++ src/main/java/de/mari_023/ae2wtlib/mixin/TrinketUniversalTerminalMixin.java
@@ -0,0 +1,16 @@
+package de.mari_023.ae2wtlib.mixin;
+
+import de.mari_023.ae2wtlib.terminal.ItemWT;
+import de.mari_023.ae2wtlib.wut.ItemWUT;
+import dev.emi.trinkets.api.SlotReference;
+import dev.emi.trinkets.api.Trinket;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.item.ItemStack;
+import org.spongepowered.asm.mixin.Mixin;
+
+@Mixin(value = ItemWUT.class, remap = false)
+public abstract class TrinketUniversalTerminalMixin extends ItemWT implements Trinket {
+    public void tick(ItemStack stack, SlotReference slot, LivingEntity entity) {
+        inventoryTick(stack, entity.getLevel(), entity, 0, false);
+    }
+}
