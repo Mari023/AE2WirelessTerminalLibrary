4e8434877039b0dc13dac4cf2b8fe636dba96ef9
Mari023 <38946771+Mari023@users.noreply.github.com>
2023-07-08 20:51:38 +0200

    use other method to encode recipes in the testplot instead of copying a privet ae2 method

--- src/main/java/de/mari_023/ae2wtlib/AE2WTLibTestPlots.java
+++ src/main/java/de/mari_023/ae2wtlib/AE2WTLibTestPlots.java
@@ -1,70 +1,24 @@
 package de.mari_023.ae2wtlib;
 
-import java.util.Arrays;
+import java.util.Objects;
 
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
 import net.minecraft.server.level.ServerLevel;
-import net.minecraft.world.inventory.TransientCraftingContainer;
 import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.item.crafting.RecipeType;
-import net.minecraft.world.level.ItemLike;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.state.properties.BlockStateProperties;
 
 import de.mari_023.ae2wtlib.wut.WUTHandler;
 import de.mari_023.ae2wtlib.wut.recipe.Common;
 
-import appeng.api.crafting.PatternDetailsHelper;
 import appeng.core.definitions.AEBlocks;
 import appeng.core.definitions.AEItems;
-import appeng.menu.AutoCraftingMenu;
+import appeng.server.testplots.CraftingPatternHelper;
 import appeng.server.testplots.TestPlot;
 import appeng.server.testworld.PlotBuilder;
 
 public class AE2WTLibTestPlots {
-    /**
-     * Copied from ae2 <a href=
-     * "https://github.com/AppliedEnergistics/Applied-Energistics-2/blob/6ba7da1ce8fd98e9ec5ce65e8d0d16fcde9c3fd3/src/main/java/appeng/server/testplots/AutoCraftingTestPlots.java#L212">...</a>
-     * TODO PR: make this public
-     */
-    private static ItemStack encodeCraftingPattern(ServerLevel level,
-            Object[] ingredients,
-            boolean allowSubstitutions,
-            boolean allowFluidSubstitutions) {
-
-        // Allow a mixed input of items or item stacks as ingredients
-        var stacks = Arrays.stream(ingredients)
-                .map(in -> {
-                    if (in instanceof ItemLike itemLike) {
-                        return new ItemStack(itemLike);
-                    } else if (in instanceof ItemStack itemStack) {
-                        return itemStack;
-                    } else if (in == null) {
-                        return ItemStack.EMPTY;
-                    } else {
-                        throw new IllegalArgumentException("Unsupported argument: " + in);
-                    }
-                })
-                .toArray(ItemStack[]::new);
-
-        var c = new TransientCraftingContainer(new AutoCraftingMenu(), 3, 3);
-        for (int i = 0; i < stacks.length; i++) {
-            c.setItem(i, stacks[i]);
-        }
-
-        var recipe = level.getRecipeManager().getRecipeFor(RecipeType.CRAFTING, c, level).orElseThrow();
-
-        var result = recipe.assemble(c, level.registryAccess());
-
-        return PatternDetailsHelper.encodeCraftingPattern(
-                recipe,
-                stacks,
-                result,
-                allowSubstitutions,
-                allowFluidSubstitutions);
-    }
-
     @TestPlot("wireless_terminal")
     public static void wireless_terminal(PlotBuilder plot) {
         var o = BlockPos.ZERO;
@@ -103,16 +57,21 @@ public class AE2WTLibTestPlots {
         plot.hopper("1 3 0", Direction.DOWN, stack1);
         plot.creativeEnergyCell("1 2 1");
         plot.blockEntity("1 2 0", AEBlocks.MOLECULAR_ASSEMBLER,
-                molecularAssembler -> molecularAssembler.getInternalInventory().setItemDirect(10, encodeCraftingPattern(
-                        (ServerLevel) molecularAssembler.getLevel(), new Object[] { stack1, stack2 }, false, false)));
+                molecularAssembler -> {
+                    ServerLevel level = (ServerLevel) Objects.requireNonNull(molecularAssembler.getLevel());
+                    molecularAssembler.getInternalInventory().setItemDirect(10,
+                            CraftingPatternHelper.encodeShapelessCraftingRecipe(level, stack1, stack2));
+                });
 
         plot.hopper("1 1 0", Direction.WEST);
         plot.hopper("0 2 0", Direction.DOWN, stack3);
         plot.creativeEnergyCell("0 1 1");
         plot.blockEntity("0 1 0", AEBlocks.MOLECULAR_ASSEMBLER,
-                molecularAssembler -> molecularAssembler.getInternalInventory().setItemDirect(10, encodeCraftingPattern(
-                        (ServerLevel) molecularAssembler.getLevel(), new Object[] { universalStack, stack3 }, false,
-                        false)));
+                molecularAssembler -> {
+                    ServerLevel level = (ServerLevel) Objects.requireNonNull(molecularAssembler.getLevel());
+                    molecularAssembler.getInternalInventory().setItemDirect(10,
+                            CraftingPatternHelper.encodeShapelessCraftingRecipe(level, universalStack, stack3));
+                });
         plot.hopper("0 0 0", Direction.DOWN);
 
         plot.test(test -> test.assertTrue(test.countContainerContentAt(BlockPos.ZERO).isEmpty(),
