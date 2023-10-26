f6d8c466d0f8274973e57e849f62ccd8b82b0964
Mari023 <38946771+Mari023@users.noreply.github.com>
2023-10-26 15:12:33 +0200

    more documentation, courtesy of ChatGPT

--- src/main/java/de/mari_023/ae2wtlib/wut/WUTHandler.java
+++ src/main/java/de/mari_023/ae2wtlib/wut/WUTHandler.java
@@ -27,8 +27,24 @@ import appeng.hotkeys.HotkeyActions;
 import appeng.menu.locator.MenuLocator;
 import appeng.menu.locator.MenuLocators;
 
+/**
+ * This class handles functionalities related to the Wireless Universal Terminal (WUT). It provides methods to get and
+ * set the current terminal, check for a specific terminal, cycle through terminals, update client terminals, and
+ * register new terminals.
+ */
 public class WUTHandler {
+    /**
+     * Definitions for all wireless terminals.
+     */
+    public static final Map<String, WTDefinition> wirelessTerminals = new HashMap<>();
+    public static final List<String> terminalNames = new ArrayList<>();
 
+    /**
+     * Get the current terminal from the provided item stack.
+     *
+     * @param terminal The ItemStack which represents the terminal.
+     * @return The current terminal name if found, or an empty string.
+     */
     public static String getCurrentTerminal(ItemStack terminal) {
         if (terminal.getItem() instanceof ItemWUT) {
             String currentTerminal = terminal.getOrCreateTag().getString("currentTerminal");
@@ -50,7 +66,15 @@ public class WUTHandler {
         return "";
     }
 
-    public static void setCurrentTerminal(Player playerEntity, MenuLocator locator, ItemStack itemStack,
+    /**
+     * Set the current terminal of a given item stack.
+     *
+     * @param player    The player.
+     * @param locator   The menu locator.
+     * @param itemStack The ItemStack to update.
+     * @param terminal  The terminal name to set.
+     */
+    public static void setCurrentTerminal(Player player, MenuLocator locator, ItemStack itemStack,
             String terminal) {
         if (!(itemStack.getItem() instanceof ItemWUT))
             return;
@@ -58,9 +82,16 @@ public class WUTHandler {
             return;
         assert itemStack.getTag() != null;
         itemStack.getTag().putString("currentTerminal", terminal);
-        updateClientTerminal((ServerPlayer) playerEntity, locator, itemStack.getTag());
+        updateClientTerminal((ServerPlayer) player, locator, itemStack.getTag());
     }
 
+    /**
+     * Checks if the given terminal item stack contains the specified terminal name.
+     *
+     * @param terminal     The terminal ItemStack.
+     * @param terminalName The terminal name to check.
+     * @return true if the terminal contains the specified terminal name, false otherwise.
+     */
     public static boolean hasTerminal(ItemStack terminal, String terminalName) {
         if (terminal.isEmpty())
             return false;
@@ -74,7 +105,15 @@ public class WUTHandler {
         return terminal.getItem().equals(wirelessTerminals.get(terminalName).item());
     }
 
-    public static void cycle(Player playerEntity, MenuLocator locator, ItemStack itemStack,
+    /**
+     * Cycles to the next terminal in the item stack based on user input.
+     *
+     * @param player               The player.
+     * @param locator              The menu locator.
+     * @param itemStack            The terminal ItemStack to update.
+     * @param isHandlingRightClick If true, cycles to the previous terminal; otherwise, cycles to the next.
+     */
+    public static void cycle(Player player, MenuLocator locator, ItemStack itemStack,
             boolean isHandlingRightClick) {
         if (itemStack.getTag() == null)
             return;
@@ -93,13 +132,28 @@ public class WUTHandler {
             nextTerminal = terminalNames.get(i);
         } while (!itemStack.getTag().getBoolean(nextTerminal));
         itemStack.getTag().putString("currentTerminal", nextTerminal);
-        updateClientTerminal((ServerPlayer) playerEntity, locator, itemStack.getTag());
+        updateClientTerminal((ServerPlayer) player, locator, itemStack.getTag());
     }
 
-    public static void updateClientTerminal(ServerPlayer playerEntity, MenuLocator locator, @Nullable CompoundTag tag) {
-        ServerNetworkManager.sendToClient(playerEntity, new UpdateWUTPackage(locator, tag));
+    /**
+     * Sends an update to the client about the current terminal.
+     *
+     * @param player  The server player.
+     * @param locator The menu locator.
+     * @param tag     The compound tag containing terminal data.
+     */
+    public static void updateClientTerminal(ServerPlayer player, MenuLocator locator, @Nullable CompoundTag tag) {
+        ServerNetworkManager.sendToClient(player, new UpdateWUTPackage(locator, tag));
     }
 
+    /**
+     * Opens a terminal menu for the player.
+     *
+     * @param player               The player.
+     * @param locator              The menu locator.
+     * @param returningFromSubmenu Set to true when the player is returning from a submenu.
+     * @return true if the terminal can be opened, false otherwise.
+     */
     public static boolean open(final Player player, final MenuLocator locator, boolean returningFromSubmenu) {
         WTMenuHost host = locator.locate(player, WTMenuHost.class);
         if (host == null)
@@ -117,6 +171,13 @@ public class WUTHandler {
                 returningFromSubmenu);
     }
 
+    /**
+     * Finds a specific terminal for a player.
+     *
+     * @param player       The player.
+     * @param terminalName The name of the terminal to find.
+     * @return A MenuLocator for the terminal if found; null otherwise.
+     */
     @Nullable
     public static MenuLocator findTerminal(Player player, String terminalName) {
         MenuLocator locator = Platform.findTerminalFromAccessory(player, terminalName);
@@ -130,6 +191,13 @@ public class WUTHandler {
         return null;
     }
 
+    /**
+     * Gets the ItemStack from a locator for a given player.
+     *
+     * @param player  The player.
+     * @param locator The menu locator.
+     * @return The ItemStack located, or an empty stack if not found.
+     */
     public static ItemStack getItemStackFromLocator(Player player, MenuLocator locator) {
         ItemStack stack = Platform.getItemStackFromTrinketsLocator(player, locator);
         if (!stack.isEmpty())
@@ -140,9 +208,17 @@ public class WUTHandler {
         return host.getItemStack();
     }
 
-    public static final Map<String, WTDefinition> wirelessTerminals = new HashMap<>();
-    public static final List<String> terminalNames = new ArrayList<>();
-
+    /**
+     * Registers a new terminal.
+     *
+     * @param name              Terminal's name.
+     * @param open              Container opener for the terminal.
+     * @param WTMenuHostFactory The factory for creating WTMenuHost.
+     * @param menuType          The menu type for the terminal.
+     * @param item              The item representing the terminal.
+     * @param hotkeyName        The hotkey name for the terminal.
+     * @param itemID            The item ID for the terminal.
+     */
     public static void addTerminal(String name, WTDefinition.ContainerOpener open,
             WTDefinition.WTMenuHostFactory WTMenuHostFactory,
             MenuType<?> menuType, IUniversalWirelessTerminalItem item, String hotkeyName, String itemID) {
@@ -161,18 +237,44 @@ public class WUTHandler {
         terminalNames.add(name);
     }
 
+    /**
+     * Registers a new terminal. Helper for terminals which follow the "wireless_" + name + "_terminal" scheme for
+     * hotkeyName
+     *
+     * @param name              Terminal's name.
+     * @param open              Container opener for the terminal.
+     * @param WTMenuHostFactory The factory for creating WTMenuHost.
+     * @param menuType          The menu type for the terminal.
+     * @param item              The item representing the terminal.
+     * @param itemID            The item ID for the terminal.
+     */
     public static void addTerminal(String name, WTDefinition.ContainerOpener open,
             WTDefinition.WTMenuHostFactory WTMenuHostFactory,
             MenuType<?> menuType, IUniversalWirelessTerminalItem item, String itemID) {
         addTerminal(name, open, WTMenuHostFactory, menuType, item, "wireless_" + name + "_terminal", itemID);
     }
 
+    /**
+     * Registers a new terminal with the handler. Helper for terminals which follow the "wireless_" + name + "_terminal"
+     * scheme for hotkeyName and the "item.ae2wtlib.wireless_" + name + "_terminal" scheme for the itemID
+     *
+     * @param name              Terminal's name.
+     * @param open              Container opener for the terminal.
+     * @param WTMenuHostFactory The factory for creating WTMenuHost.
+     * @param menuType          The menu type for the terminal.
+     * @param item              The item representing the terminal.
+     */
     public static void addTerminal(String name, WTDefinition.ContainerOpener open,
             WTDefinition.WTMenuHostFactory WTMenuHostFactory,
             MenuType<?> menuType, IUniversalWirelessTerminalItem item) {
         addTerminal(name, open, WTMenuHostFactory, menuType, item, "item.ae2wtlib.wireless_" + name + "_terminal");
     }
 
+    /**
+     * Calculates the Maximum UpgradeCardCount based on the known terminals. Each terminal adds two upgrade slots.
+     *
+     * @return The maximum amount of upgrades a Universal Terminal can have
+     */
     public static int getUpgradeCardCount() {
         return terminalNames.size() * 2;
     }
