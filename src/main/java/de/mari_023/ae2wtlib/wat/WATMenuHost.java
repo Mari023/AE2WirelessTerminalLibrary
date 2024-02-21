c2415716c460ad23e2c825b7cbfbb7db63db9406
Mari023 <38946771+Mari023@users.noreply.github.com>
2024-02-21 19:07:37 +0100

    update ae2

--- src/main/java/de/mari_023/ae2wtlib/wat/WATMenuHost.java
+++ src/main/java/de/mari_023/ae2wtlib/wat/WATMenuHost.java
@@ -2,6 +2,8 @@ package de.mari_023.ae2wtlib.wat;
 
 import java.util.function.BiConsumer;
 
+import appeng.api.networking.IGridNode;
+import appeng.api.storage.IPatternAccessTermMenuHost;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
 
@@ -11,8 +13,9 @@ import appeng.menu.locator.ItemMenuHostLocator;
 
 import de.mari_023.ae2wtlib.AE2wtlibItems;
 import de.mari_023.ae2wtlib.terminal.WTMenuHost;
+import org.jetbrains.annotations.Nullable;
 
-public class WATMenuHost extends WTMenuHost {
+public class WATMenuHost extends WTMenuHost implements IPatternAccessTermMenuHost {
     public WATMenuHost(WirelessTerminalItem item, Player player, ItemMenuHostLocator locator,
             BiConsumer<Player, ISubMenu> returnToMainMenu) {
         super(item, player, locator, returnToMainMenu);
@@ -21,5 +24,10 @@ public class WATMenuHost extends WTMenuHost {
     @Override
     public ItemStack getMainMenuIcon() {
         return new ItemStack(AE2wtlibItems.instance().PATTERN_ACCESS_TERMINAL);
+    }
+
+    @Override
+    public @Nullable IGridNode getGridNode() {
+        return getActionableNode();
     }
 }
