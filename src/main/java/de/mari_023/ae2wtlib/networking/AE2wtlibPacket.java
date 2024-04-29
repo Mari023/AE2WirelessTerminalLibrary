a46cbadf7273f895549b58a087b9a2934ab82710
Mari023 <38946771+Mari023@users.noreply.github.com>
2024-04-30 00:43:59 +0200

    fix outdated javadoc

--- src/main/java/de/mari_023/ae2wtlib/networking/AE2wtlibPacket.java
+++ src/main/java/de/mari_023/ae2wtlib/networking/AE2wtlibPacket.java
@@ -5,8 +5,7 @@ import net.minecraft.world.entity.player.Player;
 
 public interface AE2wtlibPacket extends CustomPacketPayload {
     /**
-     * processes the packet data. the buffer has been provided by the constructor, and shouldn't be released by this
-     * method
+     * processes the packet data.
      * 
      * @param player the player that send the packet
      */
