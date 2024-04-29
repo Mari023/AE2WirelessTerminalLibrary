fad47abbf80c50366faa0f29616d0e39d92de45e
Mari023 <38946771+Mari023@users.noreply.github.com>
2024-04-30 01:04:02 +0200

    synchronize UpgradeHelper methods, you never know what parallel modloading might do...

--- src/main/java/de/mari_023/ae2wtlib/UpgradeHelper.java
+++ src/main/java/de/mari_023/ae2wtlib/UpgradeHelper.java
@@ -12,10 +12,10 @@ import appeng.core.localization.GuiText;
 import de.mari_023.ae2wtlib.wut.WUTHandler;
 
 public class UpgradeHelper {
-    private static boolean readyForUpgrades = false;
+    private static volatile boolean readyForUpgrades = false;
     private static final Map<ItemLike, Integer> upgrades = new HashMap<>();
 
-    static void addUpgrades() {
+    static synchronized void addUpgrades() {
         addUpgradeToAllTerminals(AEItems.ENERGY_CARD, 0);
         addUpgradeToAllTerminals(AE2wtlibItems.instance().QUANTUM_BRIDGE_CARD, 1);
 
@@ -32,7 +32,7 @@ public class UpgradeHelper {
      * @param upgradeCard  upgrade
      * @param maxSupported how many upgrades of this type a terminal can have, 0 for maximum
      */
-    public static void addUpgradeToAllTerminals(ItemLike upgradeCard, int maxSupported) {
+    public static synchronized void addUpgradeToAllTerminals(ItemLike upgradeCard, int maxSupported) {
         if (!readyForUpgrades) {
             upgrades.put(upgradeCard, maxSupported);
             return;
