626be2f9cf871d0cb19280bd3537157a7883b304
Mari023 <38946771+Mari023@users.noreply.github.com>
2024-05-10 03:32:31 +0200

    fix color format being wrong

--- src/main/java/de/mari_023/ae2wtlib/datagen/ColorMap.java
+++ src/main/java/de/mari_023/ae2wtlib/datagen/ColorMap.java
@@ -3,16 +3,37 @@ package de.mari_023.ae2wtlib.datagen;
 import appeng.api.util.AEColor;
 
 public record ColorMap(AEColor color) {
-    private static final int blackVariant = 0xFF000000;
-    private static final int mediumVariant = 0xFF888888;
-    private static final int whiteVariant = 0xFFFFFFFF;
+    private static final int BLACK_VARIANT = 0xFF000000;
+    private static final int MEDIUM_VARIANT = 0xFF888888;
+    private static final int WHITE_VARIANT = 0xFFFFFFFF;
 
     public int map(int argb) {
         return switch (argb) {
-            case blackVariant -> color().blackVariant;
-            case mediumVariant -> color().mediumVariant;
-            case whiteVariant -> color().whiteVariant;
+            case BLACK_VARIANT -> rgbToABGR(color().blackVariant);
+            case MEDIUM_VARIANT -> rgbToABGR(color().mediumVariant);
+            case WHITE_VARIANT -> rgbToABGR(color().whiteVariant);
             default -> argb;
         };
+    }
+
+    private static int rgbToABGR(int rgb) {
+        return fromArgb(0xFF, getRrgb(rgb), getGrgb(rgb), getBrgb(rgb));
+    }
+
+    public static int getRrgb(int rgb) {
+        return (rgb >> 16) & 0xFF;
+    }
+
+    public static int getGrgb(int rgb) {
+        return (rgb >> 8) & 0xFF;
+    }
+
+    public static int getBrgb(int rgb) {
+        return rgb & 0xFF;
+    }
+
+    public static int fromArgb(int a, int r, int g, int b) {
+        //return (r << 24) | (g << 16) | (b << 8) | a;
+        return (a << 24) | (b << 16) | (g << 8) | r;
     }
 }
