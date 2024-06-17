54809694859cdf12f129852bd7fadcecff54ed33
Mari023 <38946771+Mari023@users.noreply.github.com>
2024-06-17 14:20:21 +0200

    change how Magnet Card Hotkey gets concatenated to allow translations to change the order

--- src/main/java/de/mari_023/ae2wtlib/TextConstants.java
+++ src/main/java/de/mari_023/ae2wtlib/TextConstants.java
@@ -20,14 +20,14 @@ public final class TextConstants {
     public static final MutableComponent RESTOCK_OFF = Component.translatable("gui.ae2wtlib.restock")
             .append(Component.translatable("gui.ae2wtlib.off").setStyle(STYLE_RED));
 
-    public static final MutableComponent HOTKEY_MAGNETCARD_INVENTORY = Component
-            .translatable("gui.ae2wtlib.magnetcard.hotkey")
-            .append(Component.translatable("gui.ae2wtlib.magnetcard.desc.inv").setStyle(STYLE_GREEN));
-    public static final MutableComponent HOTKEY_MAGNETCARD_ME = Component.translatable("gui.ae2wtlib.magnetcard.hotkey")
-            .append(Component.translatable("gui.ae2wtlib.magnetcard.desc.me").setStyle(STYLE_GREEN));
-    public static final MutableComponent HOTKEY_MAGNETCARD_OFF = Component
-            .translatable("gui.ae2wtlib.magnetcard.hotkey")
-            .append(Component.translatable("gui.ae2wtlib.magnetcard.desc.off").setStyle(STYLE_RED));
+    public static final MutableComponent HOTKEY_MAGNETCARD_INVENTORY = Component.translatable(
+            "gui.ae2wtlib.magnetcard.hotkey",
+            Component.translatable("gui.ae2wtlib.magnetcard.desc.inv").setStyle(STYLE_GREEN));
+    public static final MutableComponent HOTKEY_MAGNETCARD_ME = Component.translatable("gui.ae2wtlib.magnetcard.hotkey",
+            Component.translatable("gui.ae2wtlib.magnetcard.desc.me").setStyle(STYLE_GREEN));
+    public static final MutableComponent HOTKEY_MAGNETCARD_OFF = Component.translatable(
+            "gui.ae2wtlib.magnetcard.hotkey",
+            Component.translatable("gui.ae2wtlib.magnetcard.desc.off").setStyle(STYLE_RED));
     public static final MutableComponent MAGNETCARD_OFF = Component.translatable("gui.ae2wtlib.magnetcard").append("\n")
             .append(Component.translatable("gui.ae2wtlib.magnetcard.desc.off"));
     public static final MutableComponent MAGNETCARD_INVENTORY = Component.translatable("gui.ae2wtlib.magnetcard")
