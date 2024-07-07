4a70be804e4587c247517880d0b2ebd997d8d50e
Mari023 <38946771+Mari023@users.noreply.github.com>
2024-07-07 15:05:10 +0200

    spotless

--- api/src/main/java/de/mari_023/ae2wtlib/api/gui/package-info.java
+++ api/src/main/java/de/mari_023/ae2wtlib/api/gui/package-info.java
@@ -3,7 +3,7 @@
 @MethodsReturnNonnullByDefault
 package de.mari_023.ae2wtlib.api.gui;
 
+import javax.annotation.ParametersAreNonnullByDefault;
+
 import net.minecraft.FieldsAreNonnullByDefault;
 import net.minecraft.MethodsReturnNonnullByDefault;
-
-import javax.annotation.ParametersAreNonnullByDefault;
