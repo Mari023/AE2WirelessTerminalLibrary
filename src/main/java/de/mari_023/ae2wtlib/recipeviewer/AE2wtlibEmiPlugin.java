f047c0e654126d32ebdda820e4b2575bdbb1e279
Mari023 <38946771+Mari023@users.noreply.github.com>
2024-07-13 03:35:53 +0200

    neoforge is stupid and loads the config to late, so we have to fill the wireless universal terminal later

--- src/main/java/de/mari_023/ae2wtlib/recipeviewer/AE2wtlibEmiPlugin.java
+++ src/main/java/de/mari_023/ae2wtlib/recipeviewer/AE2wtlibEmiPlugin.java
@@ -1,14 +1,18 @@
 package de.mari_023.ae2wtlib.recipeviewer;
 
+import net.minecraft.world.item.ItemStack;
+
 import dev.emi.emi.api.EmiEntrypoint;
 import dev.emi.emi.api.EmiPlugin;
 import dev.emi.emi.api.EmiRegistry;
 import dev.emi.emi.api.recipe.VanillaEmiRecipeCategories;
 import dev.emi.emi.api.stack.EmiStack;
 
+import appeng.api.config.Actionable;
 import appeng.integration.modules.emi.EmiEncodePatternHandler;
 import appeng.integration.modules.emi.EmiUseCraftingRecipeHandler;
 
+import de.mari_023.ae2wtlib.AE2wtlibItems;
 import de.mari_023.ae2wtlib.wct.WCTMenu;
 import de.mari_023.ae2wtlib.wet.WETMenu;
 import de.mari_023.ae2wtlib.wut.WTDefinitions;
@@ -21,7 +25,11 @@ public class AE2wtlibEmiPlugin implements EmiPlugin {
                 new EmiEncodePatternHandler<>(WETMenu.class));
         registry.addRecipeHandler(WCTMenu.TYPE, new EmiUseCraftingRecipeHandler<>(WCTMenu.class));
 
+        ItemStack wut = WTDefinitions.CRAFTING.universalTerminal();
+        AE2wtlibItems.UNIVERSAL_TERMINAL.injectAEPower(wut,
+                AE2wtlibItems.UNIVERSAL_TERMINAL.getAEMaxPower(wut), Actionable.MODULATE);
+
         registry.addWorkstation(VanillaEmiRecipeCategories.CRAFTING,
-                EmiStack.of(WTDefinitions.CRAFTING.universalTerminal()));
+                EmiStack.of(wut));
     }
 }
