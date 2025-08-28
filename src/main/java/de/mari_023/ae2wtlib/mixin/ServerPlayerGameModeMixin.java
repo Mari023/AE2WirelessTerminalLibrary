35cf25592aa1bd995309dd3f3e3776abc2a80eb0
Mari023 <38946771+Mari023@users.noreply.github.com>
2025-08-28 23:37:25 +0200

    spotless

--- src/main/java/de/mari_023/ae2wtlib/mixin/ServerPlayerGameModeMixin.java
+++ src/main/java/de/mari_023/ae2wtlib/mixin/ServerPlayerGameModeMixin.java
@@ -1,6 +1,10 @@
 package de.mari_023.ae2wtlib.mixin;
 
-import de.mari_023.ae2wtlib.AE2wtlibEvents;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.server.level.ServerPlayerGameMode;
 import net.minecraft.world.InteractionHand;
@@ -8,16 +12,14 @@ import net.minecraft.world.InteractionResult;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.phys.BlockHitResult;
-import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+import de.mari_023.ae2wtlib.AE2wtlibEvents;
 
 @Mixin(ServerPlayerGameMode.class)
 public class ServerPlayerGameModeMixin {
-
     @Inject(method = "useItemOn", at = @At(value = "RETURN"))
-    private static void handle(ServerPlayer player, Level level, ItemStack original, InteractionHand hand, BlockHitResult hitResult, CallbackInfoReturnable<InteractionResult> cir) {
+    private static void handle(ServerPlayer player, Level level, ItemStack original, InteractionHand hand,
+            BlockHitResult hitResult, CallbackInfoReturnable<InteractionResult> cir) {
         AE2wtlibEvents.restock(player, original.copy(), player.getItemInHand(hand).getCount(),
                 (stack -> player.setItemInHand(hand, stack)));
     }
